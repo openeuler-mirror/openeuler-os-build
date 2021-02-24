80affdb08d97e1d1886550fe7e3461eb70fb832d
miao_kaibo <miaokaibo@outlook.com>
2021-02-24 17:52:15 +0800

    change method for getting epol rpms

--- script/step/get_epol_rpms.sh
+++ script/step/get_epol_rpms.sh
@@ -28,22 +28,34 @@ function get_epol_rpms_inchroot()
     TIME=${TIME#"${version}"-}
     CUSTOM_DIR="${TIME_DIR}"
     set +e
-    RELEASE_DIR="${release_dir}/EPOL/$ARCH/Packages"
-    RELEASE_SOURCE_DIR="${release_dir}/EPOL/source/Packages"
+    mv /etc/yum.repos.d /etc/yum.repos.d.bak
+    mkdir -p /etc/yum.repos.d /tmp/EPOL/${ARCH}/Packages "/tmp/EPOL/source/Packages"
+    yum-config-manager --add-repo "${OBS_EPOL_REPO_URL}" --add-repo "${OBS_BRINGINRELY_URL}"
+    yum clean all
+    RELEASE_DIR="${release_dir}/EPOL"
     SSH_CMD="mkdir -p ${RELEASE_DIR}"
     sshcmd "${SSH_CMD}"
-    SSH_CMD="mkdir -p ${RELEASE_SOURCE_DIR}"
-    sshcmd "${SSH_CMD}"
-    SSH_CMD="cd ${RELEASE_DIR} && wget -c -r -np -nd ${OBS_EPOL_REPO_URL}/${ARCH}/ && rm -rf ${ARCH} && rm -rf index.html*"
-    sshcmd "${SSH_CMD}"
-    SSH_CMD="cd ${RELEASE_DIR} && wget -c -r -np -nd ${OBS_EPOL_REPO_URL}/noarch/ && rm -rf noarch && rm -rf index.html*"
-    sshcmd "${SSH_CMD}"
-    SSH_CMD="cd ${RELEASE_SOURCE_DIR} && wget -c -r -np -nd ${OBS_EPOL_REPO_URL}/src/ && rm -rf src && rm -rf index.html*"
-    sshcmd "${SSH_CMD}"
-    SSH_CMD="cd ${RELEASE_DIR} && wget -c -r -np -nd ${OBS_BRINGINRELY_URL}/${ARCH}/ && rm -rf ${ARCH} && rm -rf index.html*"
-    sshcmd "${SSH_CMD}"
+
+    yum list --installroot="/tmp/EPOL/aarch64/Packages" --available | awk '{print $1}' | grep -E "noarch|${ARCH}" | grep -v ".src" > ava_epol_lst
+
+    yumdownloader --installroot="/tmp/EPOL/${ARCH}/Packages" --destdir="/tmp/EPOL/${ARCH}/Packages" $(cat ava_epol_lst | tr '\n' ' ')
+    rm -rf /tmp/EPOL/${ARCH}/Packages/var
+    createrepo -d /tmp/EPOL/${ARCH}
+    sshscp "/tmp/EPOL/${ARCH}" "${RELEASE_DIR}"
+    if [[ "$ARCH" == "aarch64" ]];then
+        rm -rf /etc/yum.repos.d/*
+        yum-config-manager --add-repo "${OBS_EPOL_REPO_URL}"
+        yum clean all
+        yum list --installroot="/tmp/EPOL/aarch64/Packages" --available | awk '{print $1}' | grep -E "noarch|${ARCH}" | grep -v ".src" > ava_epol_lst
+        yumdownloader --installroot="/tmp/EPOL/source/Packages" --destdir="/tmp/EPOL/source/Packages" --source $(cat ava_epol_lst | tr '\n' ' ')
+        rm -rf /tmp/EPOL/source/Packages/var
+        createrepo -d /tmp/EPOL/source
+        sshscp "/tmp/EPOL/source" "${RELEASE_DIR}"
+    fi
+    rm -rf /tmp/EPOL
+    rm -rf /etc/yum.repos.d
+    mv /etc/yum.repos.d.bak /etc/yum.repos.d
     set -e
-    SSH_CMD="cd ${release_dir} && createrepo -d ${release_dir}/EPOL/$ARCH; createrepo -d ${release_dir}/EPOL/source"
-    sshcmd "${SSH_CMD}"
+
     return 0
 }
